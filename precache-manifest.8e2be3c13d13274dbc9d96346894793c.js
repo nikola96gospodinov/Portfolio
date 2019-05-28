6df4fb82917ec178d27f7c89c2f4b949874706ab
Nikola Gospodinov <nikola96gospodinov@gmail.com>
2019-05-28 22:42:15 +0100

    Updates

---
+++ precache-manifest.8e2be3c13d13274dbc9d96346894793c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9d5b5605cfa1b0c8c30",
+    "url": "/Portfolio/static/css/main.9bb641df.chunk.css"
+  },
+  {
+    "revision": "e9d5b5605cfa1b0c8c30",
+    "url": "/Portfolio/static/js/main.d6b2cc5a.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "fb94fe1c4f9f4b864752",
+    "url": "/Portfolio/static/js/2.08a1d382.chunk.js"
+  },
+  {
+    "revision": "39758cfcb42ab4a93f659dd632613d7b",
+    "url": "/Portfolio/static/media/logo_transparent.39758cfc.png"
+  },
+  {
+    "revision": "a13d3e4ea75af2c18c74a1f44873db3b",
+    "url": "/Portfolio/static/media/hero.a13d3e4e.jpg"
+  },
+  {
+    "revision": "797f876b170393987f8e09839a40176d",
+    "url": "/Portfolio/static/media/heroAbout.797f876b.jpg"
+  },
+  {
+    "revision": "d6e246960d900b51064427fd7db85f4b",
+    "url": "/Portfolio/static/media/SkillsHero.d6e24696.jpg"
+  },
+  {
+    "revision": "5d09d1188e79207d39004a26f7a9b794",
+    "url": "/Portfolio/static/media/Story.5d09d118.jpg"
+  },
+  {
+    "revision": "03272cbb98ac1e3a4d4ba14287ac4637",
+    "url": "/Portfolio/static/media/projectHero.03272cbb.jpg"
+  },
+  {
+    "revision": "f30a28da49889a97a96d4ef60c7bdaef",
+    "url": "/Portfolio/static/media/THC.f30a28da.jpg"
+  },
+  {
+    "revision": "638b601c6569d918437872e37491ef53",
+    "url": "/Portfolio/static/media/Portfolio.638b601c.jpg"
+  },
+  {
+    "revision": "3444cfd085b28005d9f567058444761c",
+    "url": "/Portfolio/static/media/noteapp.3444cfd0.jpg"
+  },
+  {
+    "revision": "d8ccde348f755e5dcfc0b752c815ae27",
+    "url": "/Portfolio/static/media/aboutMe.d8ccde34.jpg"
+  },
+  {
+    "revision": "1f9d777af963feaa52e93fdc3e97922d",
+    "url": "/Portfolio/index.html"
+  }
+];
